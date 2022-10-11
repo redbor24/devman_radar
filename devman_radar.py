3c3709d48c4affa461a95de1cd9f6c2b335bc8b3
redbor <redbor24@gmail.com>
2022-10-11 13:05:12 +0300

    Send logs to Telegram to admin user

--- devman_radar.py
+++ devman_radar.py
@@ -2,11 +2,23 @@ import argparse
 import logging
 import time
 from datetime import datetime
+from logging.handlers import RotatingFileHandler
 
 import requests
 import telegram
 from environs import Env
 from requests.exceptions import ConnectionError, ReadTimeout
+
+LOG_FILE_MAX_LEN = 10000000
+LOG_FORMAT = '%(asctime)s %(levelname)s:%(message)s'
+LOG_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+
+class AdminLogsHandler(logging.Handler):
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        tg_bot.send_message(chat_id=tg_admin, text=log_entry)
 
 
 def format_answers(devman_answers):
@@ -27,29 +39,32 @@ if __name__ == '__main__':
     env = Env()
     env.read_env()
 
-    logging.basicConfig(
-        filename='devman_radar.log', encoding='utf-8', level=logging.INFO,
-        format='%(asctime)s %(levelname)s:%(message)s', datefmt='%Y-%m-%d %H:%M:%S'
-    )
-    logging.info('Начало работы')
-    logging.info('Программа отслеживает изменения статусов работ ученика на сайте devman.org '
-                 'и оповещает об этом через Телеграм')
-    logging.info('Для завершения работы нажмите Ctrl+C или закройте окно CMD.exe')
-
     parser = argparse.ArgumentParser()
     parser.add_argument('telegram_id', type=str, help='id пользователя Телеграм')
     args = parser.parse_args()
 
-    tg_userid = args.telegram_id
+    tg_user = args.telegram_id
     tg_token = env('TG_TOKEN')
+    tg_admin = env('TG_ADMIN_USERID')
 
     devman_token = env('DEVMAM_TOKEN')
     devman_url = env('DEVMAN_URL')
 
-    bot = telegram.Bot(tg_token)
-    message = 'Мониторинг изменения статуса работ запущен'
-    logging.info(message)
-    bot.send_message(chat_id=tg_userid, text=message)
+    logger = logging.getLogger("devman_radar")
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = RotatingFileHandler(
+        filename='devman_radar.log', encoding='utf-8', maxBytes=LOG_FILE_MAX_LEN, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(fmt=LOG_FORMAT, datefmt=LOG_DATE_FORMAT))
+    file_handler.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
+
+    tg_bot = telegram.Bot(tg_token)
+    tg_handler = AdminLogsHandler()
+    tg_handler.setLevel(logging.DEBUG)
+    logger.addHandler(tg_handler)
+
+    logger.info('Мониторинг изменения статуса работ запущен')
     check_from = datetime.now().timestamp()
     while True:
         try:
@@ -64,20 +79,11 @@ if __name__ == '__main__':
                 check_from = lesson_review['timestamp_to_request']
             if lesson_review['status'] == 'found':
                 check_from = lesson_review['last_attempt_timestamp']
-                formatted_message = format_answers(lesson_review['new_attempts'])
-
-                bot.send_message(chat_id=tg_userid, text=formatted_message)
-                logging.info('Отправлено в Телеграм:')
-                logging.info(f'  Сообщение: {formatted_message}')
-                logging.info(f'  Пользователь: {tg_userid}')
+                logger.info(format_answers(lesson_review['new_attempts']))
         except ConnectionError as e:
-            logging.error(f'Ошибка соединения: {e}')
+            logger.error(f'Ошибка соединения: {e}')
             time.sleep(300)
         except ReadTimeout as e:
-            message = f'Ошибка ожидания ответа: {e}'
-            logging.error(message)
-            bot.send_message(chat_id=tg_userid, text=message)
+            logger.error(f'Ошибка ожидания ответа: {e}')
         except Exception as e:
-            message = f'Прочие ошибки: {e}'
-            logging.error(message)
-            bot.send_message(chat_id=tg_userid, text=message)
+            logger.error(f'Прочие ошибки: {e}')
